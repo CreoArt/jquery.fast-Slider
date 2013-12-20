1b2a244bea6883a40c22b4060a3f72d62c303cfd
Mike Pol <mikle.sol@gmail.com>
2013-12-20 15:48:31 +0400

    update

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
 	$('#demo1').Slider();
     $('#demo2').Slider({type:'gallary'});
-    $('#demo3').Slider();
+    $('#demo3').Slider({circle:true,bt_hide:true});
 });
